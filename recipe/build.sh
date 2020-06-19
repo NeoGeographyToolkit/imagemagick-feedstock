f5302cdef28caa1c17f0cfe0dfa19d16398fb7d8
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-06-19 15:53:42 -0700

    Modify more dependencies

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,12 @@
 
 export CFLAGS="-I$PREFIX/include $CFLAGS"
 export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
+export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib $LDFLAGS"
 
 if [[ $(uname) == Darwin ]]; then
   export CC=clang
   export CXX=clang++
-  export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib -headerpad_max_install_names $LDFLAGS"
+  export LDFLAGS="$LDFLAGS -headerpad_max_install_names"
   export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
   export MACOSX_DEPLOYMENT_TARGET="10.9"
   export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
@@ -21,17 +22,18 @@ find $PREFIX -name '*.la' -delete
             --disable-docs \
             --disable-static \
             --disable-openmp \
-            --with-bzlib=yes \
+            --with-bzlib=no \
             --with-autotrace=no \
             --with-djvu=no \
             --with-dps=no \
-            --with-fftw=yes \
+            --with-fftw=no\
             --with-flif=no \
             --with-fpx=no \
             --with-fontconfig=no \
             --with-freetype=no \
             --with-gslib=no \
             --with-gvc=no \
+            --with-heic=no \
             --with-jbig=no \
             --with-jpeg=yes \
             --with-lcms=no \
@@ -40,7 +42,7 @@ find $PREFIX -name '*.la' -delete
             --with-lzma=yes \
             --with-magick-plus-plus=yes \
             --with-openexr=no \
-            --with-openjp2=yes \
+            --with-jp2=yes \
             --with-pango=no \
             --with-perl=no \
             --with-png=yes \
@@ -54,14 +56,14 @@ find $PREFIX -name '*.la' -delete
             --with-xml=no \
             --with-zlib=yes
 
-make -j$CPU_COUNT
+make -j${CPU_COUNT}
 # FIXME:
 # The failure below seems to be associated with the option --with-gslib,
 # but I could not get to turn "yes." See the logs for more info.
 #
 # tests/wandtest.c main 5321 non-conforming drawing primitive definition `text' @ error/draw.c/DrawImage/3269`
 # make check
-make install -j$CPU_COUNT
+make install
 
 # remove libtool files
 find $PREFIX -name '*.la' -delete
